9b6fc02e81e6618ea60b9db64bcc73670dbd53f9
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-05-16 18:14:55 +0000

    remove googlemaps from theme instead of ACF

--- privatearea.php
+++ privatearea.php
@@ -457,12 +457,16 @@ function enable_acf_forms(){
     
     if( is_page( get_theme_mod( 'private_area_profile' ) ) ) {
        acf_form_head(); 
-       add_filter( "acf/settings/enqueue_google_maps", false );
+       add_action('wp_enqueue_scripts', __NAMESPACE__.'\\dequeue_google_maps');
     }
     
 }
 
 add_action( 'wp', __NAMESPACE__.'\\enable_acf_forms' );
+
+function dequeue_google_maps(){
+    wp_dequeue_script('googlemaps');
+}
 
 if ( ! wp_next_scheduled( 'svbk_privatearea_expiration_check' ) ) {
   wp_schedule_event( time(), 'hourly', 'svbk_privatearea_expiration_check' );
